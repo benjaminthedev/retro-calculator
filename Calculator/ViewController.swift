60423a0ddca4d3015face4a8037371e824949dbf
ben <MacBookAir@Benjamins-MacBook-Air.local>
2016-08-27 16:16:07 +0100

    New updates

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -18,8 +18,8 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        //This is a new comment to test it all out
-        //This is a new comment also         
+        //Need to remove this comment.
+        
     }
 
 
