ec98cfa0ca321b3221dcd7060c4d2fbd135946f3
ben <MacBookAir@Benjamins-MacBook-Air.local>
2016-08-28 08:57:38 +0100

    New files and updates

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -3,25 +3,52 @@
 //  Calculator
 //
 //  Created by Benjamin Dordoigne on 11/08/2016.
-//  Copyright © 2016 Benjamin the Geek!! Oh yeahhhhh . All rights reserved.
+//  Copyright © 2016 Benjamin the Geek! All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    //load sound for btn
+    var btnSound: AVAudioPlayer!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Audio Player for button
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL (fileURLWithPath: path!)
+        
+        //Try to play sound
+        do{
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            //prepare to play
+            btnSound.prepareToPlay()
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        //Need to remove this comment - soon
+     }
+
+    
+    //Function to play sound
+    @IBAction func numberPressed(btn: UIButton!){
+    btnSound.play()
         
+    
     }
-
-
 }
 
